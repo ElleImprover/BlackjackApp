58b964dd6970bce85b43004e99a993b95d22d9d5
Elizabeth Davis <elizabeth.davis@improving.com>
2021-07-23 13:35:37 -0500

    Added Playing method

--- BlackjackApp/CardManipulation.cs
+++ BlackjackApp/CardManipulation.cs
@@ -6,22 +6,39 @@ using System.Threading.Tasks;
 
 namespace BlackjackApp
 {
-  public class CardManipulation
+    public class CardManipulation
     {
         Bet bet { get; set; }
-        public Card DrawCard() 
+        public Card DrawCard()
         {
-           bet.ShuffleDeck();
-           return bet.DeckOfCards.Pop();
+            bet.ShuffleDeck();
+            return bet.DeckOfCards.Pop();
         }
 
+        public Card Hit()
+        {
+            return bet.DeckOfCards.Pop();
+        }
         public bool PlayBlackJack()
         {
             bool win = false;
+            while (!win)
+            {
+                Console.Out.WriteLine("Please type 'start' to play or 'x' to exit.");
+                var play = Console.ReadLine();
+                if (String.Equals(play, "start", StringComparison.CurrentCultureIgnoreCase))
+                {
+
+                }
+                else if (String.Equals(play, "x", StringComparison.CurrentCultureIgnoreCase))
+                {
+
+                }
 
 
+            }
             return win;
-        }
 
-}
+        }
+    }
 }
